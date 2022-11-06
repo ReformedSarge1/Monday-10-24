751d5865b1c8168fd0d312d1d848973f7ed71433
Carter Cook <raymanomen3014@gmail.com>
2022-11-06 10:49:20 -0800

    first

---
+++ .py
@@ -0,0 +1,29 @@
+{
+  "abilities": [
+    {
+      "ability": {
+        "name": "sand-veil",
+        "url": "https://pokeapi.co/api/v2/ability/8/"
+      },
+      "is_hidden": false,
+      "slot": 1
+    },
+    {
+      "ability": {
+        "name": "arena-trap",
+        "url": "https://pokeapi.co/api/v2/ability/71/"
+      },
+      "is_hidden": false,
+      "slot": 2
+    },
+    {
+      "ability": {
+        "name": "sand-force",
+        "url": "https://pokeapi.co/api/v2/ability/159/"
+      },
+      "is_hidden": true,
+      "slot": 3
+    }
+  ],
+  "base_experience": 53
+}
---
+++ forms.py
@@ -0,0 +1,30 @@
+forms: [
+    "name": "diglett",
+    "url": "https://pokeapi.co/api/v2/pokemon-form/50/"
+
+
+    "name": "diglett",
+    "url": "https://pokeapi.co/api/v2/pokemon-form/50/"
+
+
+game_indices: [
+      "game_index": 59,
+      version: {
+        name: "red",
+        url: "https://pokeapi.co/api/v2/version/1/"
+      }
+    {
+      game_index: 59,
+      version: {
+        name: "blue",
+        url: "https://pokeapi.co/api/v2/version/2/"
+      }
+    },
+    {
+      game_index: 59,
+      version: {
+        name: "yellow",
+        url: "https://pokeapi.co/api/v2/version/3/"
+      }
+]
+]
